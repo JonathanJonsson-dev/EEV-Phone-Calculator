94fc1174ad1bdf1d09bbc5362b34d6ec83477808
Jonathan <jojo1130@student.miun.se>
2020-11-17 16:01:23 +0100

    Add project files.

---
+++ EEV Phone Calculator/EEV Phone Calculator/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace EEV_Phone_Calculator
+{
+    public partial class MainPage : ContentPage
+    {
+
+        Button Plus, Minus, Divide, Multiply, Enter;
+
+
+        
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
